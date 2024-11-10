72175f95b24c3c64d0b85b771129736057d78668
Necrodeather <morbid6dead@gmail.com>
2024-11-10 13:17:28 +0300

    feat: add lints settings and correction for lints

--- app/infrastructure/engine.py
+++ app/infrastructure/engine.py
@@ -1,4 +1,8 @@
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.ext.asyncio import (
+    create_async_engine,
+    async_sessionmaker,
+    AsyncSession,
+)
 
 
 class SqlalchemyEngine:
--- app/infrastructure/models/budget.py
+++ app/infrastructure/models/budget.py
@@ -5,6 +5,7 @@ from sqlalchemy.orm import Mapped, relationship
 from .mixins import BaseMixin, CreatedAtMixin, UpdatedAtMixin
 from app.infrastructure.base import str_128, date_sql, decimal
 
+
 class Category(BaseMixin, CreatedAtMixin, UpdatedAtMixin):
     __abstract__ = True
 
@@ -14,7 +15,9 @@ class Category(BaseMixin, CreatedAtMixin, UpdatedAtMixin):
 class ExpensesCategory(Category):
     __tablename__ = "expenses_categories"
 
-    expenses: Mapped[list["Expenses"]] = relationship(back_populates="category")
+    expenses: Mapped[list["Expenses"]] = relationship(
+        back_populates="category",
+    )
 
 
 class IncomesCategory(Category):
@@ -30,7 +33,9 @@ class Expenses(BaseMixin, CreatedAtMixin, UpdatedAtMixin):
     category_id: Mapped[int]
     amount: Mapped[decimal]
     date: Mapped[date_sql]
-    category: Mapped["ExpensesCategory"] = relationship(back_populates="expenses")
+    category: Mapped["ExpensesCategory"] = relationship(
+        back_populates="expenses",
+    )
 
 
 class Income(BaseMixin, CreatedAtMixin, UpdatedAtMixin):
@@ -40,4 +45,6 @@ class Income(BaseMixin, CreatedAtMixin, UpdatedAtMixin):
     amount: Mapped[decimal]
     category_id: Mapped[int]
     date: Mapped[date_sql]
-    category: Mapped["IncomesCategory"] = relationship(back_populates="incomes")
+    category: Mapped["IncomesCategory"] = relationship(
+        back_populates="incomes",
+    )
